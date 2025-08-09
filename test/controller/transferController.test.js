1b02f63fbaf54f043e1df569c966d548bab40630
Jesi Freire <jesianef@hotmail.com>
2025-08-09 18:00:17 -0300

    Adicionando Mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -4,6 +4,8 @@ const {expect} = require('chai')
 
 const app = require('../../app')
 
+const transferService = require('../../services/transferService')
+
 describe('Transfer controller',()=>{
     describe('POST/transfer',()=>{
         it('Quando informo remetente e destinatario inexistentes recebo 400', async() => {
@@ -18,6 +20,24 @@ describe('Transfer controller',()=>{
             expect(resposta.body).to.have.property('error','Usuário não encontrado')
             
         })
+    })
+         it('Usando Mocks: Quando informo remetente e destinatario inexistentes recebo 400', async() => {
+    
+        const transferServiceMock = sinon.stub(transferService, 'createTransfer')
+        transferServiceMock.returns({ error: 'Usuário não encontrado' })
+
+            const resposta = await request(app)                                                                                                             
+            .post('/transfer')
+            .send({
+                from: "jesi",
+                to: "toddy",
+                amount: 10
+        })
+        expect(resposta.status).to.equal(400)
+        expect(resposta.body).to.have.property('error','Usuário não encontrado')
+
+        sinon.restore
+        
     })
 
 })
